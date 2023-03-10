fcc8b059a5ffd6cbd58a6fec0f9877b6402ca1bf
Dani Setien <dani.setien@gmail.com>
2023-03-10 22:06:08 +0100

    Add Layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import { useAppSelector } from "../../store/hooks";
+import Loader from "../LoginForm/Loader/Loader";
+
+interface LayoutProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  const { isLoading } = useAppSelector((state) => state.ui);
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
