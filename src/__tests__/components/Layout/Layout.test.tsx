d2e68e640940d60b46679c4c0428a185312f7d6c
Dani Setien <dani.setien@gmail.com>
2023-03-11 12:13:35 +0100

    Test Layout component

---
+++ src/__tests__/components/Layout/Layout.test.tsx
@@ -0,0 +1,24 @@
+import { screen } from "@testing-library/react";
+import Layout from "../../../components/Layout/Layout";
+import LoginPage from "../../../pages/login";
+import renderWithProviders from "../../../utils/testUtils/renderWithProviders";
+
+describe("Given a Layout component", () => {
+  describe("When rendered with a LoginPage inside", () => {
+    test("Then it should show the Log in Form button", () => {
+      const expectedButtonName = "Log in";
+
+      renderWithProviders(
+        <Layout>
+          <LoginPage />
+        </Layout>
+      );
+
+      const loginButton = screen.getByRole("button", {
+        name: expectedButtonName,
+      });
+
+      expect(loginButton).toBeInTheDocument();
+    });
+  });
+});
