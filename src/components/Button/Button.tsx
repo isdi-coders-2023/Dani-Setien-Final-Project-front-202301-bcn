7636f12e3fb7ef44e7d8b03fcd71c97b938da683
Dani Setien <dani.setien@gmail.com>
2023-03-12 13:09:33 +0100

    Test generic button for displayed icon

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,15 @@
 interface ButtonProps {
   icon: JSX.Element;
   disabled: boolean;
+  ariaLabel: string;
 }
 
-const Button = ({ icon, disabled }: ButtonProps): JSX.Element => {
-  return <button disabled={disabled}>{icon}</button>;
+const Button = ({ icon, disabled, ariaLabel }: ButtonProps): JSX.Element => {
+  return (
+    <button aria-label={ariaLabel} disabled={disabled}>
+      {icon}
+    </button>
+  );
 };
 
 export default Button;
