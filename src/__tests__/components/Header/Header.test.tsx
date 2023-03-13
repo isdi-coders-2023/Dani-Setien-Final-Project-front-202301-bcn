4235bf1944864f7ef81053f825da05f28e7be853
Dani Setien <dani.setien@gmail.com>
2023-03-13 11:47:53 +0100

    Test Header for rendered elements

---
+++ src/__tests__/components/Header/Header.test.tsx
@@ -0,0 +1,31 @@
+import { screen } from "@testing-library/react";
+import Header from "../../../components/Header/Header";
+import renderWithProviders from "../../../utils/testUtils/renderWithProviders";
+
+describe("Given a Header component", () => {
+  describe("When rendered", () => {
+    test("Then it should show a logo image", () => {
+      const expectedLogoName = "brushbids logo";
+
+      renderWithProviders(<Header />);
+
+      const logo = screen.getByRole("img", {
+        name: expectedLogoName,
+      });
+
+      expect(logo).toBeInTheDocument();
+    });
+
+    test("Then it should show a Log out button", () => {
+      const expectedButtonText = "Log out";
+
+      renderWithProviders(<Header />);
+
+      const logoutButton = screen.getByRole("button", {
+        name: expectedButtonText,
+      });
+
+      expect(logoutButton).toBeInTheDocument();
+    });
+  });
+});
